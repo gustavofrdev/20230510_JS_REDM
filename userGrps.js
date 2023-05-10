496eaaf9a8c81bb913a11e7f1d83d54ba354858e
Gustavo Ferreira <55332994+gustavofrdev@users.noreply.github.com>
2023-05-10 10:09:05 -0300

    Update userGrps.js
    added code!

--- userGrps.js
+++ userGrps.js
@@ -1,7 +1,28 @@
-*//xFrameworkFivem 20230510 --- GRUPS
+internal = {}
 
+function addInternal(vari, value){
+    internal[vari] = value
+}
 
-FUNÇÃO 1 ----> USER_GROUPS
+const ConfigFile = _Framework.GroupsConfig().push();
+const waitTolerance = 500;
+var API = {};
+const exec = _Framework.mysqlExecJs();
 
+decode = json.decode
 
+function userGroups(userId){
+    toleraVal =0
+    grupos = MySQL.SQLExecute('SELECT grupos FROM xframework_personagens WHERE id = @id', {['@id'] = user_id})
+    while(grupos == 0){
+        toleraVal+=1
+        if(toleraVal >= waitTolerance){
+            break;
+        }
+    }
+    if(toleraVal >= waitTolerance){
+        print("we can't continue with this code. Is it broken?")
+    }
 
+    return decode(grupos)
+}
